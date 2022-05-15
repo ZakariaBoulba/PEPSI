a6bc3a495ca7dc003a191af00a2e8541c0ef8b79
ZakariaBoulba <zakariaboulbaroud2@gmail.com>
2022-05-16 00:53:14 +0100

    create projet

---
+++ script.js
@@ -0,0 +1,15 @@
+function imgSlider(beta) {
+    document.querySelector('.pepsi').src = beta;
+}
+
+function changeBgColor(color) {
+    const sec = document.querySelector('.sec');
+    sec.style.background = color;
+} 
+
+function menuToggle() {
+    const toggleMenu = document.querySelector('.toggleMenu');
+    const navigation = document.querySelector('.navigation');
+    toggleMenu.classList.toggle('active');
+    navigation.classList.toggle('active');
+}
